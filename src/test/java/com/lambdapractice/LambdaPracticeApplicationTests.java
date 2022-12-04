056f0d14651b422eb8d71feb88c2a845db20be5a
taegeun <djqtek0525@naver.com>
2022-12-04 18:53:57 +0900

    first commit
    JPA 연결 및 Swagger 연결

---
+++ src/test/java/com/lambdapractice/LambdaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
