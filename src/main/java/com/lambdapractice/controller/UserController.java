76006a924b279b7cc4701db71c6e1101ea6484e1
taegeun <djqtek0525@naver.com>
2022-12-04 23:25:15 +0900

    [add] UserName이 중복일때 Error 발생기능 추가
    UserController에서 무조건성공하던걸, 중복시 ExceptionManager의 ExceptionHandler로 처리할 수 있게함.

--- src/main/java/com/lambdapractice/controller/UserController.java
+++ src/main/java/com/lambdapractice/controller/UserController.java
@@ -14,10 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
-    private final UserService service;
+    private final UserService userService;
 
     @PostMapping("/join")
     public ResponseEntity<String> join(@RequestBody UserJoinRequest dto) {
+        userService.join(dto.getUserName(),dto.getPassword());
         return ResponseEntity.ok().body("회원가입이 성공했습니다.");
     }
 }
