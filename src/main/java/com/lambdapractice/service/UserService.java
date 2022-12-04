3b2940d752c3304b3cc461d6ddd4b390f94e79d7
taegeun <djqtek0525@naver.com>
2022-12-04 23:59:13 +0900

    [add] 비밀번호 암호화해서 저장
    Security 디펜던시 추가
    EncoderConfig 추가
    SecurityConfig 추가
    Service에서 password 저장할 때 암호화하도록 함

--- src/main/java/com/lambdapractice/service/UserService.java
+++ src/main/java/com/lambdapractice/service/UserService.java
@@ -5,19 +5,21 @@ import com.lambdapractice.exception.AppException;
 import com.lambdapractice.exception.ErrorCode;
 import com.lambdapractice.repositroy.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    public final UserRepository userRepositroy;
+    public final UserRepository userRepository;
+    public final BCryptPasswordEncoder encoder;
 
 
     public String join(String userName, String password) {
 
         //userName 중복 check
-        userRepositroy.findByUserName(userName)
+        userRepository.findByUserName(userName)
                 .ifPresent(user -> {
                     throw new AppException(ErrorCode.USERNAME_DUPLICATED, userName + "는 이미 있습니다.");
                 });
@@ -25,9 +27,9 @@ public class UserService {
         // 저장 (나중에는 암호화해서 저장할것)
         User user = User.builder()
                 .userName(userName)
-                .password(password)
+                .password(encoder.encode(password)) //암호화 해서 저장
                 .build();
-        userRepositroy.save(user);
+        userRepository.save(user);
 
 
         return "SUCCESS";
